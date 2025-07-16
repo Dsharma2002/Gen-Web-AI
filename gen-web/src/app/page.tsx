81cfe591b8166944fe8133e2699934c2b99d3ee5
Divyansh Sharma <divyansh.abbott9712@gmail.com>
2025-07-16 14:30:06 +0100

    trpc fetch prefetch

--- gen-web/src/app/page.tsx
+++ gen-web/src/app/page.tsx
@@ -1,8 +1,20 @@
-import { caller } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Page = async () => {
-  const data = await caller.createAI({ text: "Div" });
-  return <div>{JSON.stringify(data)}</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "Div PREFETCH" })
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
