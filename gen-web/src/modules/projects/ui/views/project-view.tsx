50c254181995529bf264285aa5443be57b909122
Divyansh Sharma <divyansh.abbott9712@gmail.com>
2025-07-26 14:37:06 +0100

    UI-resize panel

--- gen-web/src/modules/projects/ui/views/project-view.tsx
+++ gen-web/src/modules/projects/ui/views/project-view.tsx
@@ -2,6 +2,11 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 interface Props {
   projectId: string;
@@ -17,9 +22,20 @@ export const ProjectView = ({ projectId }: Props) => {
   );
 
   return (
-    <div>
-      {JSON.stringify(project)}
-      {JSON.stringify(messages, null, 2)}
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          {JSON.stringify(project)}
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+          {JSON.stringify(messages, null, 2)}
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 };
